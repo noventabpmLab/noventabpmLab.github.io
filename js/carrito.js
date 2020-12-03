b99a446c167cb7e10578627b02f1fe98a3cb8cde
noventabpmLab <74113838+noventabpmLab@users.noreply.github.com>
2020-12-03 15:06:41 -0500

    fixes

--- js/carrito.js
+++ js/carrito.js
@@ -3,54 +3,60 @@ const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
 addToShoppingCartButtons.forEach((addToShoppingCartButton) => {
 	addToShoppingCartButton.addEventListener('click', addToCartClicket);
 }) ;
-
 const shopingCardItemsContainer = document.querySelector('.shoppingCartItemsContainer');
 
-
 function addToCartClicket(event){
-    const button = event.target;
-    const item = button.closest('.modal-content');
-
-    const itemPrice = item.querySelector('.itemPrice').textContent;
-    const itemImg = item.querySelector('.card-img-top').src;
-
-    addItemToshoppingCar(itemPrice, itemImg);
-    
-
+	const button = event.target;
+	const item = button.closest('.item');
+	
+	const itemTitle = item.querySelector('.card-title').textContent;
+	const itemPrice = item.querySelector('.itemPrice').textContent;
+	const itemImg = item.querySelector('.card-img-top').src;
+	
+	addItemToshoppingCar(itemTitle, itemPrice, itemImg);
 }
-function addItemToshoppingCar(itemPrice, itemImg){
+function addItemToshoppingCar(itemTitle, itemPrice, itemImg){
+	const  elementsTittle = shopingCardItemsContainer.getElementsByClassName('shoppingCartItemTitle');
+	
+	for(let i = 0; i < elementsTittle.length; i++){
+		if(elementsTittle[i].innerText === itemTitle){
+		   let elementQuantity = elementsTittle[i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity')
+		  
+		elementQuantity.value++;
+		updateShoppingCartTotal();	
+		return;
+	   
+		};
+	}
 	
 	const shopingCartRow = document.createElement('div');
 	const shopingCardItemsContent = 
-          `
-          <div class="row shoppingCartItem">
-          <div class="col-8">
-              <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                  <img  width="150px;" src=${itemImg} class="shopping-cart-image">
-                  <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-              </div>
-          </div>
-          <div class="col-3">
-              <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                  <p class="item-price mb-0 shoppingCartItemPrice"> $Col ${itemPrice} </p>
-              </div>
-          </div>
-          <div class="col-1">
-              <div
-                  class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                  <input style="width: 75px;" class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                      value="1">
-                  <button class="btn btn-danger buttonDelete" type="button">X</button>
-              </div>
-          </div>
-      </div>`
-
-          ;
+		  `<div class="row shoppingCartItem">
+                    <div class="col-8">
+                        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                            <img  width="150px;" src=${itemImg} class="shopping-cart-image">
+                            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                            <p class="item-price mb-0 shoppingCartItemPrice"> $Col ${itemPrice} </p>
+                        </div>
+                    </div>
+                    <div class="col-1">
+                        <div
+                            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                            <input style="width: 75px;" class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                                value="1">
+                            <button class="btn btn-danger buttonDelete" type="button">X</button>
+                        </div>
+                    </div>
+                </div>`;
 	
 	shopingCartRow.innerHTML = shopingCardItemsContent;
-    shopingCardItemsContainer.append(shopingCartRow);
-    
-    shopingCartRow.querySelector('.buttonDelete').addEventListener('click',removeShoppingCartItem);
+	shopingCardItemsContainer.append(shopingCartRow);
+	
+	shopingCartRow.querySelector('.buttonDelete').addEventListener('click',removeShoppingCartItem);
 	
 	shopingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change', quantityChanged);
 	
@@ -87,6 +93,4 @@ function quantityChanged(event){
 	};
 	updateShoppingCartTotal();
 	
-}
-
 }
